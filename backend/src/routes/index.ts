0ed660ffaaa367586ac93cebc99cefc06c0d3e67
Constantin Vanchurenkov <i@wacorasu.ru>
2023-08-14 14:26:05 +0300

    инициализация

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -5,15 +5,21 @@ import userRouter from './users';
 import cardRouter from './cards';
 import auth from '../middlewares/auth';
 import NotFoundError from '../errors/not-found-error';
+import { createUser, login } from '../controllers/users';
 import {
-  createUser, login,
-} from '../controllers/users';
-import { validateUserBody, validateAuthentication } from '../middlewares/validatons';
+  validateUserBody,
+  validateAuthentication,
+} from '../middlewares/validatons';
 
 const router = Router();
 router.post('/signup', validateUserBody, createUser);
 router.post('/signin', validateAuthentication, login);
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 // все роуты, кроме /signin и /signup, защищены авторизацией;
 router.use(auth);
 router.use('/users', userRouter);
