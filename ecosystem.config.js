1c142784d2bd0602da0040822464f064891258ba
Constantin Vanchurenkov <i@wacorasu.ru>
2023-08-21 17:34:36 +0300

    настройка деплоя через pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -31,7 +31,7 @@ module.exports = {
       ref: REPO_REF,
       path: DEPLOY_PATH,
       key: KEY,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'npm i && npm run build',
     },
   },
