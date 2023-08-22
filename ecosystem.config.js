01ad9fdf38bca253d35360fcf170fd51666d6145
Constantin Vanchurenkov <i@wacorasu.ru>
2023-08-22 20:45:41 +0300

    настройка деплоя через pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,7 @@ const {
   DEPLOY_USER,
   REPO_REF,
   DEPLOY_PATH,
+  NPM,
 } = dotenv.parsed;
 
 const { JWT_SECRET, DATABASE_HOST } = process.env;
@@ -15,7 +16,7 @@ module.exports = {
     {
       name: 'mesto-backend',
       script: './dist/app.js',
-      env_env_production: {
+      env_production: {
         NODE_ENV: 'production',
         DATABASE_HOST,
         secret_word: JWT_SECRET,
@@ -30,7 +31,7 @@ module.exports = {
       ref: REPO_REF,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp -C ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source`,
-      'post-deploy': 'npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': NPM,
     },
   },
 };
