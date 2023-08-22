b3bddef647c156d0f31f62e1a637f0a7467edbcd
Constantin Vanchurenkov <i@wacorasu.ru>
2023-08-22 22:17:16 +0300

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ mongoose.connect(DB_ADDRESS);
 // Только для локальных тестов. Не используйте это в продакшене
 app.use(
   cors({
-    origin: ' *',
+    origin: 'https://mesto.wacorasu.nomoreparties.co',
   }),
 );
 app.use(express.json());
